b16d0ad53469699a9d5765a7b12185d16d10c49b
unknown <saw_series@hotmail.com>
2024-03-21 13:23:44 +0700

    '21032567'

--- app/Views/page6.php
+++ app/Views/page6.php
@@ -1,10 +1,8 @@
-﻿<!doctype html>
-<html lang="en">
+﻿<html>
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>PSUs</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -13,254 +11,173 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap">
   
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap')
 
-
-a {
-  text-decoration: none;
-  color: gray;
+@font-face {
+  font-family: PSU-Stidti-Regular;
+  src: url('./font/PSUStidti/PSU-Stidti-Regular.otf');
 }
 
-*:before, *:after {
-  box-sizing: inherit;
-  font-weight: 400;
-  font-family: "Prompt", sans-serif;
-  font-size:16px;    
-  -webkit-font-smoothing: antialiased;
-/*   border: 1px solid red; */
+@font-face {
+  font-family: PSU-Stidti-Light;
+  src: url('./font/PSUStidti/PSU-Stidti-Light.otf');
 }
 
-a:hover {
-  color: black;
-}
-
-*{
-  box-sizing: border-box;
-  font-family: "Prompt", sans-serif;
-  font-size:16px;    
-  -webkit-font-smoothing: antialiased;
+@font-face {
+  font-family:PSU-Stidti-Bold;
+  src: url('./font/PSUStidti/PSU-Stidti-Bold.otf');
 }
 
 
-ul {
-  padding: 0;
-  margin: 0;
-}
-
-li {
-  list-style: none;
-}
-
-a:focus,
-a:hover {
-  text-decoration: none;
-  outline: 0;
-}
-
-.row{
-    background-color:#fff;
-}
-
-/*stricky-nav-bar*/
-
+/*Navigation Bar with Animated Dropdown*/
+/*font-https://www.facebook.com/MungmingMoror/posts/1197628123725198*/
+/*color-#193c6c,#003c71*/
 .sticky{
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     transition: max-height 0.2s ease-in;
-
 }
 
-  .navbar {
-    width: 100%;
+
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "PSU-Stidti-Regular","PSU-Stidti-Light","PSU-Stidti-Bold",sans-serif;
+}
+
+body {
+  font-family: "PSU-Stidti-Regular","PSU-Stidti-Light","PSU-Stidti-Bold",sans-serif;
+}
+
+.navbar {
     background-color: white;
-    box-shadow: 0px 0px 4px #ccc;
-    width:100%;  
-    height:70px;
-  }
-
-  .navbar-brand {
-      color: #000;
-      font-weight: bold;
-      padding:1rem;
-  }
-
-  .navbar-nav .nav-item{        
-      padding:2rem;
-      list-style: none;
-      display:block;
-      height:auto;
-      background-color:white;
-  }
-
-    .navbar-nav .nav-item .nav-link {
-      color: #999;
-      position: relative; 
-      font-weight: 400;
-      font-family: "Prompt", sans-serif;
-      font-size:16px;  
-      padding:1rem;  
-    }
-
-    .navbar-nav .nav-item .nav-link:hover {
-      background-color:#e9eef4;
-      color:black;
-      padding:1rem;
-    }
-
-
-.dropdown{  
-    top:90px;
-    position:absolute;
-    display:none;
-    background-color:#FFF;
-    -webkit-box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
-    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
-    -webkit-transition: all .5s, background, 2s .5s linear;
-    transition: all .5s, background 2s .5s linear;
-  }
-
-
-.nav-item:hover .dropdown{
-  display:block;
-}
-
-.dropdown-nav  li {
-    display:block;
-    padding:1.2rem;
-    color:black;
-    min-width: 200px;
-}
-
-.dropdown-nav  li:hover{
-  background-color:#e9eef4;
-  color:black;
+    box-shadow: 5px 0px 5px #ccc;
+    width:100vm;  
+    padding: 0px 70px;
 }
 
 
-.dropdown:hover {
-	.dropdown-nav {
-		opacity: 1;
-		box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-	}
+.navbar-toggle{
+    list-style-type: none;
+    display: flex;
+    width:auto;  
+    float:right; 
 }
 
-.dropdown-nav {
-	opacity: 0;
-	transition: transform .5s cubic-bezier(0.88,-0.72, 0, 1), opacity .3s ease-in-out;
-	transform: translateY(-4rem);
-	list-style: none;
-	margin: 0;
-	padding: 0;
-	position: absolute;
-	background: white;
-}
-
-
-.navbar-toggler {
-  display: none;
-  float:right;  
-}
-
-
-
-@media (max-width: 920px) {
-
-.navbar-brand{
-    display:block;
-}
-
-.navbar-nav .nav-item{
-    display:block;
-    width:100%;   
-}
-
-.navbar-nav{
-    width: 100%;
-    transition: all 0.3s ease-in;
-    top:0;
-    gap: 1rem;
-    width: 100vh;
-    position:relative;
-}
-
-/*.collapse .navbar-collapse{
-    top:10;
-    gap: 1rem;
-    width: 100%;
-    background-color: #1A237E;
-    height: 0;
-    transition: all 0.3s ease-in;
-    top: 40px;
-    left: 0;
-    overflow: hidden;
-}*/
-
-.navbar-toggler {
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  float:right;
-  width:100%;
-  
-}
-
-.navbar-toggler > button{
-    height: 2.5rem;
+.navbar-toggle button{
     background-color:transparent;
+    padding:0.2rem;
+    font-size:18pt;
+    border-radius:5px;
 }
 
-.nav-item:hover > .dropdown,
-.dropdown-link:hover>.dropdown {
-  transform: translateX(30px);
-  pointer-events: auto;
-  display:block;
+.navbar-logo{
+    list-style-type: none;
+    display: flex;
+    width:auto;
+}
+
+.navbar-logo li a{
+  display: block;
+  text-decoration: none;
+  color: #444;
+  padding: 20px 20px;
+  font-weight: 700;
+  transition: 0.5s all;
+}
+
+
+.navbar-links {
+  list-style-type: none;
+  display: flex;
+  width:auto;
+}
+
+.navbar-login{
+    list-style-type: none;
+    display: flex;
+    width:auto;
+}
+
+.navbar-login li a {
+  display: block;
+  text-decoration: none;
+  color: #444;
+  padding: 20px 20px;
+  font-weight: 700;
+  transition: 0.4s all;
+}
+
+.navbar-links li a {
+  display: block;
+  text-decoration: none;
+  color: #444;
+  padding: 20px 20px;
+  font-weight: 700;
+  transition: 0.4s all;
+}
+
+.navbar-links li.navbar-dropdown {
+  position: relative;
+}
+
+.navbar-links li.navbar-dropdown:hover .dropdown {
+  visibility: visible;
+  opacity: 1;
+  transform: translateY(0px);
+  display:block;  
+}
+
+.navbar-links li.navbar-dropdown .dropdown {
+  visibility: hidden;
+  opacity: 0;
+  position: absolute;
+  padding: 20px 0;
+  top:100%;
+
+  left: 0;
+  width: 250px;
+  background-color: #fff;
+  box-shadow: 0px 10px 10px 0px rgba(0, 0, 0, 0.5);
+  border-bottom-left-radius: 3px;
+  border-bottom-right-radius: 3px;
+  z-index: 111;  
 }
 
 .dropdown{
-    position:absolute;
-    background-color:white;
+    display:none;
+    transition: transform .5s cubic-bezier(0.88,-0.72, 0, 1), opacity .3s ease-in-out;
+	  transform: translateY(-4rem);
+    opacity: 0;
 }
 
-/*.dropdown .navbar-nav .dropdown-link:hover{
-padding:1rem;
-background-color:#E5E8E8;
+
+.navbar-links li.navbar-dropdown .dropdown a {
+  font-weight: 400;
+  padding:15px; 
+}
+.navbar-dropdown .dropdown a:hover {
+  padding-left: 30px;
+}
+.navbar-links li a:hover {
+  color: #4EACF8;
+  background-color:#e9eef4;
 }
 
-.flex-container {
-  background-color: #f1f1f1;
-  display:block;
+.navbar-toggle{
+    display:none;
 }
 
-.flex-container > ul {
-width:100%;
-border-left:1px solid white;
+.navbar-collapse{
+    width:auto;
 }
 
-.flex-container > ul > li {
-    display:block;
-    padding:0.5rem;
-}*/
-
-.login{
-  display:none;
-}
-
-.top-footer {
-    background-color:#003c71;color:white;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    padding-bottom: 8rem;
-    padding-top: 2rem;
-  }
-
-}
-
-/*footerr*/
-
+/*----- footer ---*/
 
 .top-footer {
     background-color:#003c71;color:white;
@@ -269,27 +186,25 @@ border-left:1px solid white;
     justify-content: space-around;
     padding-bottom: 8rem;
     padding-top: 2rem;
+    font-family: "PSU-Stidti-Regular","PSU-Stidti-Light","PSU-Stidti-Bold",sans-serif;
   }
   
 .footer-title {
   font-weight: 500;
-  font-size: 1.2rem;
+  font-size: 16pt;
   padding: 1rem 0;
-  background-image: -webkit-linear-gradient(
-    120deg,
-    var(--primary-color-alt),
-    var(--primary-color)
-  );
+  color:white;
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+  font-family: "PSU-Stidti-Regular","PSU-Stidti-Light","PSU-Stidti-Bold",sans-serif;
 }
 .footer-items h3 {
   cursor: pointer;
   font-weight: 300;
-  font-size: 1.1rem;
+  font-size:14pt;
   padding: 0.1rem 0;
-  color: var(--white-color-alt);
+  font-family: "PSU-Stidti-Regular","PSU-Stidti-Light","PSU-Stidti-Bold",sans-serif;
 }
 .footer-items h3:hover {
   text-decoration: underline;
@@ -323,125 +238,251 @@ border-left:1px solid white;
   text-decoration: underline;
 }
 
-  /* ================= MAIN ================ */
-
-  /* =============== FOOTER =============== */
 
 
-  </style>
+@media (max-width: 920px) {
+    .navbar-login{
+        display:none;
+    }
+
+    .navbar-toggle{
+      display:block;
+    }
+
+    .navbar {
+    padding: 0px;
+    }
+
+    .navbar-links{
+        width: 100vh;
+        transition: all 0.3s ease-in;
+        top:0;
+        display:block;
+    }
+
+    .navbar-links li.navbar-dropdown:hover .dropdown {
+    visibility: visible;
+    opacity: 1;
+    transform: translateY(0px);
+    display:block;
+    position:relative;
+    }
+
+    .navbar-links li.navbar-dropdown .dropdown {
+    visibility: hidden;
+    opacity: 0;
+    padding: 20px 0;
+    transform: translateY(50px);
+    left: 0;
+    background-color: #fff;
+    box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.5);
+    border-bottom-left-radius: 3px;
+    border-bottom-right-radius: 3px;
+    z-index: 111;
+    transition: 0.4s all;
+    width:100vh;
+   }
+
+   .dropdown{
+    display:none;
+    transition: transform .6s cubic-bezier(1,-0.72, 0, 1), opacity .6s ease-in-out;
+	transform: translateY(-10rem);
+    }
+
+   
+}
+
+/*-----------*/
+.box {
+    width: 200px;
+    height: 200px;
+    border: 3px solid #333;
+    background-color: #ccc;
+    float: left;
+    margin-right: 20px;
+}
+
+@-webkit-keyframes fading {
+    from {
+        opacity: 0;
+    }
+    to {
+         opacity: 1;   
+    }
+}
+@-moz-keyframes fading {
+    from {
+        opacity: 0;
+    }
+    to {
+         opacity: 1;   
+    }
+}
+
+@keyframes fading {
+    from {
+        opacity: 0;
+    }
+    to {
+         opacity: 1;   
+    }
+}
+
+.invisible1 {
+    opacity: 0;
+    webkit-animation: fading ease-in 2s;
+    animation: fading ease-in 2s;
+    -webkit-animation: fading ease-in 2s;
+    animation: fading ease-in 2s;
+    -webkit-animation-fill-mode: forwards;
+    animation-fill-mode: forwards;
+    -webkit-animation-duration: 2s;
+    animation-duration: 2s;
+}
+
+.invisible1 .one {
+
+}
+
+.invisible1 .two {
+    -webkit-animation-delay: 2.1s;
+    animation-delay: 2.1s;
+  
+}
+
+.invisible1 .three {
+    -webkit-animation-delay: 4.3s;
+    animation-delay: 4.3s;
+ 
+}
+
+
+@keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+.fade-in-text {
+  font-family: Arial;
+  font-size: 150px;
+  animation: fadeIn 5s;
+}
+
+</style>
 <script>
-
-$(window).scroll(function(){
+    $(window).scroll(function(){
             if($(this).scrollTop() > 100){
                 $('.navbar').addClass('sticky')
             } else{
                 $('.navbar').removeClass('sticky')
             }
         });
-
-$( document ).ready(function() {
-  $(".navbar-nav .nav-item .nav-link").hover(function(){
-  
-    $('#dropdown').slideToggle(280);
-    
-     //$(".dropdown").css("display", "block");
-     //$(".dropdown").fadeIn();
-  }, function(){
-    //$(".dropdown").css("display", "none");
-  //$(this).css("background-color", "pink");
-   });
-});
-       
-
 </script>
-  </head>
-  <body>
-<!--<nav class="top-nav-bar">
-  <a class="brand">
-  <img src="./image/logo.png" />
-  </a>
-  <a class="link" href="#">login</a>
-  <a class="link" href="#">logout</a>
-</nav>-->
+</head>
+<body>
+<div class="navbar">
+      <ul class="navbar-logo" style="float:left;">
+      <li class="navlogo"><img src="./image/logo.png" height="60px" /></li>     
+      </ul>
 
-<nav class="navbar navbar-dark">
-  <div class="container">
-    <!-- Brand/logo -->
-    <a class="navbar-brand" href="#">
-      <img src="./image/logo.png" height="55px" />
-    </a>
-
-    <div class="navbar-toggler">
-    <button  type="button" style="" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="fa fa-bars"></span>
-    </button>
-    </div>
-    <!-- Navbar links -->
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-      <ul class="navbar-nav" style="float:left;">
-        <li class="nav-item">
-          <a class="nav-link" href="#">หน้าแรก</a>
-        </li>
-        <li class="nav-item" style="background-color:red;"> 
-          <a class="nav-link" href="#">เกี่ยวกับเรา v</a>          
-          <div class="dropdown" id="dropdown">
-                <ul class="dropdown-nav">
-                  <li class="dropdown-link">
-                    <a href="#">Link 1</a>
-                  </li>
-                  <li class="dropdown-link">
-                    <a href="#">Link 2</a>
-                  </li>
-                  <li class="dropdown-link">
-                    <a href="#">Link 4</a>
-                  </li>
-                  <li class="dropdown-link">
-                    <a href="#">Link 5</a>
-                  </li>
-                </ul>               
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">บริการ v</a>
-          <div class="dropdown">
-          <ul class="dropdown-nav">
-					<li><a href="">Lawn Care</a></li>
-					<li><a href="">Walling &amp; Fencing</a></li>
-					<li><a href="">Landscape design</a></li>
-					<li><a href="">Grounds Maintenance</a></li>
-				 </ul>
-         </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">หลักสูตร v</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">ติดต่อ</a>
+      <ul class="navbar-toggle" style="float:right;">
+        <li>
+            <a><button  type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="fa fa-bars"></span>
+                </button>
+            </a>
         </li>
       </ul>
-      <ul class="login navbar-nav" style="float:right;">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
+
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-links" style="float:left;">  
+        <li class="navbar-dropdown">
+          <a href="#">เกี่ยวกับเรา</a> 
+          <div class="dropdown">
+            <a href="#">Tomato Soup</a>
+            <a href="#">Veg Manchow Soup</a>
+            <a href="#">Veg Hot Soup</a>
+          </div>
         </li>
-      </ul>  
-    </div>       
-  </div>
-</nav>
-
-
-
+        <li class="navbar-dropdown">
+          <a href="#">หลักสูตร</a>
+          <div class="dropdown">
+            <a href="#">Ladoo</a>
+            <a href="#">Besan Ladoo</a>
+            <a href="#">Ghee Besan Ladoo</a>
+            <a href="#">Nariyal Ladoo</a>
+            <a href="#">Kaju Katli</a>
+            <a href="#">Rasgulla</a>
+          </div>
+        </li>
+        <li class="navbar-dropdown">
+          <a href="#">ติดต่อ</a>
+          <div class="dropdown">
+            <a href="#">Lachha Paratha</a>
+            <a href="#">Rumali Roti</a>
+            <a href="#">Tandoori Roti</a>
+            <a href="#">Butter Roti</a>
+            <a href="#">Plain Naan</a>
+            <a href="#">Butter Naan</a>
+          </div>
+        </li>
+        <li class="navbar-dropdown">
+          <a href="#">ผู้เกี่ยวข้อง</a>
+          <div class="dropdown">
+            <a href="#">Plain Rice</a>
+            <a href="#">Veg Pulao</a>
+            <a href="#">Veg Biryani</a>
+            <a href="#">Paneer Biryani</a>
+            <a href="#">Lemon Rice</a>
+            <a href="#">Veg Kashmiri Pulao</a>
+          </div>
+        </li>
+        <li class="navbar-dropdown">
+          <a href="#">Chinese</a>
+          <div class="dropdown">
+            <a href="#">Paneer Chill Dry</a>
+            <a href="#">Paneer Garlic</a>
+            <a href="#">Veg Garlic</a>
+            <a href="#">Veg Chilli</a>
+          </div>
+        </li>
+      </ul>
+      <ul class="navbar-login" style="float:right;">
+        <li><a href="#">Login</a></li>
+      </ul>
+    </div>
+</div>
+   
 <div class="container">
   <div class="row">
 <center>
+<div class="fade-in-text">
   <img src="./image/img2.png" width="90%" style="text-align:center;" />
+</div>
+</center>
+
+<center>
+    <p>Delay fade in on load</p>
+<div class="box invisible1 one">1</div>
+<div class="box invisible1 two">2</div>
+<div class="box invisible1 three">3</div>
     </center>
-    <h2 class="link"><a>new blog</a></h2>
+
+
+
+   <br><br><br><br><br><br><br><br><br> 
+   
+  <div class="fade-in-text">
+    <p>Fade-in Text</p>
+  </div>
+   
+   <h2 class="link"><a>new blog</a></h2>
     <h2 class="link"><a>sweet blog</a></h2>
-    <h2 class="link"><a>sweeter blog</a></h2>    <h2 class="link"><a>new blog</a></h2>
+    <h2 class="link"><a>sweeter blog</a></h2>    
     <h2 class="link"><a>sweet blog</a></h2>
-    <h2 class="link"><a>sweeter blog</a></h2>    <h2 class="link"><a>new blog</a></h2>
+    <h2 class="link"><a>sweeter blog</a></h2>    
     <h2 class="link"><a>sweet blog</a></h2>
-    <h2 class="link"><a>sweeter blog</a></h2>    <h2 class="link"><a>new blog</a></h2>
+    <h2 class="link"><a>sweeter blog</a></h2>   
     <h2 class="link"><a>sweet blog</a></h2>
     <h2 class="link"><a>sweeter blog</a></h2>
   </div>
@@ -496,9 +537,6 @@ $( document ).ready(function() {
       <div class="copyright">copyright © 2021 - Present • <b>DOWNTOWN TECH LIVER</b></div>
       <a class="designer" href="#">Thierry M</a>
     </div>
-  
- 
-</body>
+
+
 </html>
-
-
